4f483ab011a15e72576d0b7ee520d22ae347b1e7
EnderXie23 <3142007828@qq.com>
2025-05-06 10:09:17 +0800

    Adjust stable audio scripts

---
+++ stable-audio-tools/scripts/train.sh
@@ -0,0 +1,4 @@
+python ./train.py --config-file /home/xmy/DiffusionTunes/stable-audio-tools/scripts/train_info.ini \
+    --dataset-config /home/xmy/DiffusionTunes/stable-audio-tools/scripts/train_dataset.json \
+    --model-config /nvme0n1/xmy/stable-audio/model_config.json \
+    --name diffusion_tunes
